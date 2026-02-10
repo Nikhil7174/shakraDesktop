ffc37ac781f1c78ea64acd260a296ae07b1b4018
nikhil7174 <nikhilkumarsingh7174@gmail.com>
2026-02-10 21:02:00 +0530

    minor ui change in searchbar

--- src/renderer/src/pages/CandidateDashboard.tsx
+++ src/renderer/src/pages/CandidateDashboard.tsx
@@ -183,7 +183,7 @@ export const CandidateDashboard: React.FC = () => {
   const columns = useMemo(() => [
     {
       title: (
-        <div style={{ height: 32, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ height: 32, display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
           {companySearchVisible ? (
             <Input
               placeholder="Search company..."
@@ -195,7 +195,7 @@ export const CandidateDashboard: React.FC = () => {
               autoFocus
               prefix={<SearchOutlined style={{ color: '#9CA3AF' }} />}
               onClick={(e) => e.stopPropagation()}
-              style={{ width: '100%', fontSize: 13 }}
+              style={{ width: '70%', fontSize: 13 }}
             />
           ) : (
             <div
@@ -235,7 +235,7 @@ export const CandidateDashboard: React.FC = () => {
     },
     {
       title: (
-        <div style={{ height: 32, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ height: 32, display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
           {titleSearchVisible ? (
             <Input
               placeholder="Search interview..."
@@ -247,7 +247,7 @@ export const CandidateDashboard: React.FC = () => {
               autoFocus
               prefix={<SearchOutlined style={{ color: '#9CA3AF' }} />}
               onClick={(e) => e.stopPropagation()}
-              style={{ width: '100%', fontSize: 13 }}
+              style={{ width: '70%', fontSize: 13 }}
             />
           ) : (
             <div
